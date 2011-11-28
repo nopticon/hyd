bdabf9613eef246c929c706bb4b255625ea36b8e
Nopticon <nopticon@gmail.com>
2011-11-28 15:49:35 -0600

    ch

--- cover.php
+++ cover.php
@@ -17,9 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 define('IN_NUCLEO', true);
-require_once('./interfase/common.php');
+//require_once('./interfase/common.php');
 
-_pre('here1', true);
+die('here1', true);
 
 $user->init();
 $user->setup();
