ee573c0d557c27051f75506fbedc2ea95ddae1f0
Nopticon <info@nopticon.com>
2012-01-02 16:41:37 -0600

    fix

--- interfase/cover.php
+++ interfase/cover.php
@@ -46,6 +46,10 @@ class cover {
 		foreach ($news as $i => $row) {
 			if (!$i) $template->assign_block_vars('news', array());
 			
+			if ($user->data['is_founder']) {
+				echo $config['news_path'] . $row['news_id'] . '.jpg<br />';
+			}
+			
 			$news_image = (@file_exists($config['news_path'] . $row['news_id'] . '.jpg')) ? $row['news_id'] : 'd';
 			
 			$template->assign_block_vars('news.row', array(
