7a96bd0fb451c353a45c52f960a953c4167e03ab
Nopticon <info@nopticon.com>
2012-01-02 09:40:17 -0600

    fix

--- interfase/session.php
+++ interfase/session.php
@@ -48,11 +48,11 @@ class session {
 			$this->session_id = request_var($config['cookie_name'] . '_sid', '');
 		}
 		
-		if ($this->ip == '190.149.104.129') {
+		if ($this->ip == '173.245.56.207') {
 			//_pre($_REQUEST);
 			//_pre($_COOKIE, true);
-			//_pre($this->cookie_data['u'] . '*');
-			//_pre($this->session_id . '*', true);
+			_pre($this->cookie_data['u'] . '*');
+			_pre($this->session_id . '*', true);
 		}
 		
 		// Is session_id is set
