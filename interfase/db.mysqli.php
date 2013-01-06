38edae2165b72638919880b4ea2b37abf12d2dea
Nopticon <info@nopticon.com>
2013-01-06 05:41:49 -0600

    htda test 2

--- interfase/db.mysqli.php
+++ interfase/db.mysqli.php
@@ -28,8 +28,6 @@ class database extends dcom {
 		$this->connect = new mysqli($this->_access['server'], $this->_access['login'], $this->_access['secret'], $this->_access['database']);
 		
 		if ($this->connect->connect_error) {
-			print_r($this->_access);
-			echo $this->connect->connect_error;
 			exit('330');
 		}
 		unset($this->_access);
