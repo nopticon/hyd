f4bc5a19916f5ffccbd69a3325a7ffba344e83c1
Nopticon <info@nopticon.com>
2013-04-05 12:54:51 -0600

    Test webhook

--- interfase/draft/import_genres.php
+++ interfase/draft/import_genres.php
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 define('IN_APP', true);
-
 define('ROOT', '../../');
 require_once(ROOT . 'interfase/common.php');
 
