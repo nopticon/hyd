06a7d28bfcb06ce54ec7cb0509e7a5dbad26a40b
Guillermo Azurdia <info@nopticon.com>
2018-05-02 16:43:35 -0500

    - Fix artist acp selection

--- core/acp/artist_select.php
+++ core/acp/artist_select.php
@@ -17,20 +17,21 @@ class __artist_select extends mac {
             redirect(s_link('acp', [$redirect, 'a' => $artist]));
         }
 
-        $artist_select = '';
+        $sql_artist = '';
         if (!$user->is('founder')) {
-            $ub = _implode(',', sql_rowset(sql_filter($sql, $user->d('user_id')), false, 'ub'));
             $sql = 'SELECT ub
                 FROM _artists_auth
                 WHERE user_id = ?';
-            $artist_select = ' WHERE ub IN (' . $ub . ') ';
+            $ub = sql_rowset(sql_filter($sql, $user->d('user_id')), false, 'ub');
+
+            $sql_artist = ' WHERE ub IN (' . _implode(',', $ub) . ') ';
         }
 
         $sql = 'SELECT ub, subdomain, name
             FROM _artists
             ??
             ORDER BY name';
-        $artists = sql_rowset(sql_filter($sql, $artist_select));
+        $artists = sql_rowset(sql_filter($sql, $sql_artist));
 
         foreach ($artists as $i => $row) {
             if (!$i) {
